5d4fb3b03fadacc84c3e7eb1192a97fe21f4dedd
doligez <doligez>
2012-05-09 15:46:29 +0000

    label version 0.7.1 for CASC competition

--- versionnum.ml
+++ versionnum.ml
@@ -1,14 +1,14 @@
 (*  Copyright 2008 INRIA  *)
-Version.add "$Id: versionnum.ml,v 1.88 2012-04-24 17:32:04 doligez Exp $";;
+Version.add "$Id: versionnum.ml,v 1.89 2012-05-09 13:46:29 doligez Exp $";;
 
 open Printf;;
 
-let number = 250;;      (* strictly increasing *)
-let date = "2012-04-24";;
+let number = 251;;      (* strictly increasing *)
+let date = "2012-05-09";;
 
 let major = 0;;
 let minor = 7;;
-let bugfix = 0;;
+let bugfix = 1;;
 
 let short = sprintf "%d.%d.%d" major minor bugfix;;
 let full = sprintf "%d.%d.%d [a%d] %s" major minor bugfix number date;;
