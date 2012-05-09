690a94642c8bf1e98314ee2f6deef622cd7cfb36
doligez <doligez>
2012-05-09 15:55:39 +0000

    set version num post-release

--- versionnum.ml
+++ versionnum.ml
@@ -1,14 +1,14 @@
 (*  Copyright 2008 INRIA  *)
-Version.add "$Id: versionnum.ml,v 1.89 2012-05-09 13:46:29 doligez Exp $";;
+Version.add "$Id: versionnum.ml,v 1.90 2012-05-09 13:55:39 doligez Exp $";;
 
 open Printf;;
 
-let number = 251;;      (* strictly increasing *)
+let number = 252;;      (* strictly increasing *)
 let date = "2012-05-09";;
 
 let major = 0;;
 let minor = 7;;
-let bugfix = 1;;
+let bugfix = 2;;
 
 let short = sprintf "%d.%d.%d" major minor bugfix;;
 let full = sprintf "%d.%d.%d [a%d] %s" major minor bugfix number date;;
