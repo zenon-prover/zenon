0881d7eac8a2d8be870a1df1c30eac994e3a59ae
Guillaume Bury <guillaume.bury@gmail.com>
2014-05-06 10:46:58 +0200

    fof is now "typechecked".

--- typetptp.ml
+++ typetptp.ml
@@ -1,4 +1,6 @@
 
+open Expr
+
 type env = {
     dummy : int;
 }
@@ -20,7 +22,31 @@ let map_fold f s l =
 let type_tff_expr env e = e, env
 
 (* TODO: check that all variables have no type, i.e type Namespace.univ_name  *)
-let type_fof_expr e = ()
+let rec type_fof_expr e = match e with
+    | Evar _
+    | Etrue
+    | Efalse ->
+            ()
+    | Emeta (v, _) ->
+            type_fof_expr v
+    | Eapp (_, l, _) ->
+            List.iter type_fof_expr l
+    | Enot (e', _) ->
+            type_fof_expr e'
+    | Eand (e', e'', _)
+    | Eor (e', e'', _)
+    | Eimply (e', e'', _)
+    | Eequiv (e', e'', _) ->
+            type_fof_expr e';
+            type_fof_expr e''
+    | Eall (e', t, e'', _)
+    | Eex (e', t, e'', _)
+    | Etau (e', t, e'', _)
+    | Elam (e', t, e'', _) ->
+            if t <> Namespace.univ_name then
+                Error.err "Typed variable in untyped formula.";
+            type_fof_expr e';
+            type_fof_expr e''
 
 let type_phrase env p = match p with
     | Phrase.Formula (name, kind, e) ->
