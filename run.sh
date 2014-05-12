5d2f43664bf731e5040a8babab3ec83c06336164
Guillaume Bury <guillaume.bury@gmail.com>
2014-05-12 13:46:44 +0200

    Completed TODO

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #/bin/bash -e
 
+# hard coded path, please forgive me
 FILES=/home/guigui/build/tptp/Problems/ARI/*
 
 for f in $FILES
@@ -7,14 +8,11 @@ do
     ./zenon -itptp $f > /dev/null 2> /dev/null
     if [ $? -eq 0 ];
     then
-        echo -e "\e[32m[PROOF FOUND] $f \e[0m"
-    fi
-    if [ $? -eq 12 ];
+        echo -e "\e[31m[PROOF FOUND] $f \e[0m"
+    elif [ $? -eq 12 ] || [ $? -eq 1 ];
     then
-        echo -e "\e[35m[SYNTAX OK] $f \e[0m"
-    fi
-    if ! [ $? -eq 0 ] && ! [ $? -eq 12 ];
-    then
+        echo -e "\e[32m[PROOF NOT FOUND] $f \e[0m" > /dev/null
+    else
         echo -e "\e[31m[ERROR $?] $f \e[0m"
     fi
 done
