1295662c4ef4ede4036413ae6d195b1ac8bd2ca0
Guillaume Bury <guillaume.bury@gmail.com>
2014-05-14 17:40:43 +0200

    Arith extension architecture done.

--- ext_arith.ml
+++ ext_arith.ml
@@ -1,25 +1,134 @@
 
+(* TODO:
+ * - finish wrapper function aroun ssimplex
+ * - put equations in normal form whenever we encounter one (mainly the ones we add in a node)
+*)
+
 open Expr
 open Node
 open Mlproof
-(* module S = Simplex.Make(struct type t = Expr.t let compare = Expr.compare end) *)
+module S = Simplex.Make(struct type t = Expr.t let compare = Expr.compare end)
+
+(* Manipulation of expressions/formulas *)
+exception NotaFormula
+
+let rec fadd_aux (c, x) = function
+    | [] -> [(c, x)]
+    | (c', y) :: r ->
+            if equal x y then
+                (Q.add c c', x) :: r
+            else
+                (c', y) :: (fadd_aux (c, x) r)
+
+let fadd a b = List.fold_left (fun e c -> fadd_aux c e) a b
+let fdiff a b = fadd a (List.map (fun (c, x) -> (Q.neg c, x)) b)
+let fmul c a = List.map (fun (c', x) -> (Q.mul c c', x)) a
+
+let normalize a b =
+    let rec pop_const = function
+        | [] -> (Q.zero, [])
+        | (c, x) :: r ->
+                if equal etrue x then
+                    c, r
+                else
+                    let c', r' = pop_const r in
+                    c', (c, x) :: r'
+    in
+    let c = fdiff a b in
+    pop_const c
+
+let of_cexpr = function
+    | Evar (s, _) -> Q.of_string s
+    | _ -> raise NotaFormula
+
+let rec of_nexpr = function
+    | Eapp (("$int"|"$rat"), [v], _) -> [of_cexpr v, etrue]
+    | Etau (_, ("$int"|"$rat"), _, _) as a -> [Q.one, a]
+    | Eapp ("$sum", [a; b], _) -> fadd (of_nexpr a) (of_nexpr b)
+    | Eapp ("$difference", [a; b], _) -> fdiff (of_nexpr a) (of_nexpr b)
+    | Eapp ("product", [Eapp (("$int"|"$rat"), [v], _); a], _)
+    | Eapp ("product", [a; Eapp (("$int"|"$rat"), [v], _)], _) ->
+            fmul (of_cexpr v) (of_nexpr a)
+    | _ -> raise NotaFormula
+
+let of_bexpr = function
+    | Eapp (("$lesseq"|"$greatereq"|"$eq_$int"|"$eq_$rat") as s, [a; b], _ ) ->
+            let a', b' = of_nexpr a, of_nexpr b in
+            let e, c = normalize a' b' in
+            (e, s, c)
+    | _ -> raise NotaFormula
+
 
 (* Helper around the simplex module *)
 type simplex = {
-    (* core : S.t; *)
+    core : S.t;
     bindings : (expr * expr) list;
 }
 
-(* Internal state *)
-type state = {
-    stack : (expr * simplex) Stack.t;
-    todo : (expr * Node.node_item) list; (* could be a map ? *)
+let simplex_empty = {
+    core = S.empty;
+    bindings = [];
 }
 
+let simplex_add s f =
+    s
+
+let simplex_solve s =
+    false, []
+
+(* Internal state *)
+type state = {
+    mutable solved : bool;
+    stack : (expr * simplex) Stack.t;
+    mutable todo : (expr * Node.node_item) list; (* could be a map ? *)
+}
+
+let empty_state = {
+    solved = false;
+    stack = Stack.create ();
+    todo = [];
+}
+
+let st_pop st =
+    ignore (Stack.pop st.stack);
+    st.solved <- false;
+    st.todo <- []
+
+let st_head st =
+    try
+        snd (Stack.top st.stack)
+    with Stack.Empty -> simplex_empty
+
+let st_push st x = Stack.push x st.stack
+
+let st_is_head st e =
+    try
+        let e', _ = Stack.top st.stack in
+        equal e e'
+    with Stack.Empty -> false
+
+let st_set st (b, res) =
+    st.solved <- b;
+    st.todo <- st.todo @ res
+
 (* Extension functions *)
-let add_formula, remove_formula, to_do =
-    let f _ = () in
-    f, f, f
+let add_expr, remove_expr, to_do =
+    let st = empty_state in
+    let is_new e = not (List.exists (fun (e', _) -> equal e e') st.todo) in
+    let add e = (* try and compute a solution *)
+        if is_new e && not st.solved then begin
+            try
+                let f = of_bexpr e in
+                let s = st_head st in
+                let s' = simplex_add s f in
+                st_push st (e, s');
+                st_set st (simplex_solve s')
+            with NotaFormula -> ()
+        end
+    and remove e = if st_is_head st e then st_pop st
+    and to_do e = snd (List.find (fun (e', _) -> equal e e') st.todo)
+    in
+    add, remove, to_do
 
 (* TODO: rewrite these *)
 let lesseq a b = eapp ("$lesseq", [a; b])
@@ -27,7 +136,10 @@ let greatereq a b = eapp ("$greatereq", [a; b])
 let minus_one e = eapp ("$difference", [e; eapp ("$int", [evar "1"])])
 let plus_one e = eapp ("$sum", [e; eapp ("$int", [evar "1"])])
 
-let newnodes e g _ = match e with
+let newnodes e g _ =
+    try
+        [to_do e]
+    with Not_found -> begin match e with
     | Enot (Eapp ("$eq_$int", [a; b], _), _) -> [Node {
         nconc = [e];
         nrule = Ext ("arith", "neq", [a; b]);
@@ -35,6 +147,7 @@ let newnodes e g _ = match e with
         ngoal = g;
         nbranches = [| [lesseq a (minus_one b)]; [greatereq a (plus_one b)] |]; }]
     | _ -> []
+    end
 
 let make_inst term g = assert false
 
@@ -51,8 +164,8 @@ Extension.register {
   Extension.name = "arith";
   Extension.newnodes = newnodes;
   Extension.make_inst = make_inst;
-  Extension.add_formula = add_formula;
-  Extension.remove_formula = remove_formula;
+  Extension.add_formula = add_expr;
+  Extension.remove_formula = remove_expr;
   Extension.preprocess = (fun x -> x);
   Extension.add_phrase = (fun _ -> ());
   Extension.postprocess = (fun x -> x);
