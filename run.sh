9f271bd64f0ae093c99aa47c2524ce955e0d0373
Guillaume Bury <guillaume.bury@gmail.com>
2014-05-15 17:06:00 +0200

    Fixed bug in run.sh test utility

--- run.sh
+++ run.sh
@@ -8,26 +8,28 @@ FILES2=test/arith-*.f
 for f in $FILES1
 do
     ./zenon -x arith -itptp $f > /dev/null 2> /dev/null
-    if [ $? -eq 0 ];
+    RET=$?
+    if [ $RET -eq 0 ];
     then
         echo -e "\e[32m[PROOF FOUND] $f \e[0m"
-    elif [ $? -eq 12 ] || [ $? -eq 1 ];
+    elif [ $RET -eq 12 ] || [ $RET -eq 1 ];
     then
-        echo -e "\e[31m[PROOF NOT FOUND] $f \e[0m"
+        echo -e "\e[31m[NOT FOUND] $f \e[0m"
     else
-        echo -e "\e[31m[ERROR $?] $f \e[0m"
+        echo -e "\e[35m[ERROR $RET] $f \e[0m"
     fi
 done
 for f in $FILES2
 do
     ./zenon -x arith -itptp $f > /dev/null 2> /dev/null
-    if [ $? -eq 0 ];
+    RET=$?
+    if [ $RET -eq 0 ];
     then
         echo -e "\e[31m[PROOF FOUND] $f \e[0m"
-    elif [ $? -eq 12 ] || [ $? -eq 1 ];
+    elif [ $RET -eq 12 ] || [ $RET -eq 1 ];
     then
-        echo -e "\e[32m[PROOF NOT FOUND] $f \e[0m"
+        echo -e "\e[32m[NOT FOUND] $f \e[0m"
     else
-        echo -e "\e[31m[ERROR $?] $f \e[0m"
+        echo -e "\e[35m[ERROR $?] $f \e[0m"
     fi
 done
