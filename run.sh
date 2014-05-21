3c75d1c1c754b725c3706b99a1b4bf4cd32512d9
Guillaume Bury <guillaume.bury@gmail.com>
2014-05-21 16:23:06 +0200

    Test script a little bit cleaner.

--- run.sh
+++ run.sh
@@ -1,20 +1,25 @@
 #/bin/bash -e
 
 # hard coded path, please forgive me
-TEMP=./temp.gv
+TEMP=./temp42.gv
 FILES=/home/guigui/build/tptp/Problems/ARI/*.p
 
 # Generate 'interesting' problem list :
 # grep -L "real" /home/guigui/build/tptp/Problems/ARI/*.p | xargs grep -l "0 type" | xargs grep -l "0   ?" | xargs grep -l "Theorem" > arith_pbs
 
-rm /home/guigui/build/tptp/Problems/ARI/*.ps
+TOTAL=0
+SOLVED=0
+
 for f in $FILES
+#for f in `grep -L "real" /home/guigui/build/tptp/Problems/ARI/*.p | xargs grep -l "0 type" | xargs grep -l "0   ?" | xargs grep -l "Theorem"`
 do
+    TOTAL=$(($TOTAL + 1))
     ./zenon -x arith -itptp $f > /dev/null 2> /dev/null
     RET=$?
     if [ $RET -eq 0 ];
     then
         echo -e "\e[32m[PROOF FOUND] $f \e[0m"
+        SOLVED=$(($SOLVED + 1))
         ./zenon -q -odot -x arith -itptp $f > $TEMP
         dot -Tps $TEMP -o $f.ps
     elif [ $RET -eq 12 ] || [ $RET -eq 1 ];
@@ -25,5 +30,6 @@ do
         ./zenon -x arith -itptp $f
     fi
 done
-echo `ls -l /home/guigui/build/tptp/Problems/ARI/*.ps | wc -l` / `ls -l /home/guigui/build/tptp/Problems/ARI/*.p | wc -l`
+echo "$SOLVED / $TOTAL"
+
 rm $TEMP
