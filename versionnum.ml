93c7eff2ff02aad6257e7bbbb6dad214ed22782e
Francois Pessaux <francois_pessaux@yahoo.fr>
2016-05-31 10:40:37 +0200

    [Install] Default lib dir is now in a "zenon" directory to ensure properly working with Coq > 8.5 loadpath mechanism."

--- versionnum.ml
+++ versionnum.ml
@@ -3,12 +3,12 @@ Version.add "$Id$";;
 
 open Printf;;
 
-let number = 263;;      (* strictly increasing *)
-let date = "2014-10-21";;
+let number = 264;;      (* strictly increasing *)
+let date = "2016-05-31";;
 
 let major = 0;;
 let minor = 8;;
-let bugfix = 0;;
+let bugfix = 1;;
 
 let short = sprintf "%d.%d.%d" major minor bugfix;;
 let full = sprintf "%d.%d.%d [a%d] %s" major minor bugfix number date;;
